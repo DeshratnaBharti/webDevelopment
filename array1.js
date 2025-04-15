db2a6adf0fd7f07ad02925d7eecd71ebc4c4cfbf
Deshratna Bharti <133379373+DeshratnaBharti@users.noreply.github.com>
2025-04-15 20:41:43 +0630

    Create array1.js

---
+++ array1.js
@@ -0,0 +1,13 @@
+// your code goes here
+arr=[1,2,3,4,5];
+console.log(arr);//[ 1, 2, 3, 4, 5 ]
+arr.push(9);//push at the back;
+console.log(arr);//[ 1, 2, 3, 4, 5, 9 ]
+arr.pop();//remove last element
+console.log(arr);///[ 1, 2, 3, 4, 5 ]
+arr.unshift(6);//add element to the Array
+console.log(arr);//[ 6, 1, 2, 3, 4, 5 ]
+arr.shift();//remove first element
+console.log(arr); //[ 1, 2, 3, 4, 5 ]
+
+

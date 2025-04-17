b8e4dff0104ae02c2d6488ed66e1b38657044d79
Deshratna Bharti <133379373+DeshratnaBharti@users.noreply.github.com>
2025-04-17 09:08:14 +0630

    Create string1.js

---
+++ string1.js
@@ -0,0 +1,10 @@
+// your code goes here
+let s="coder";
+console.log(s,s.length);
+for(let i=0;i<s.length;i++){
+    console.log(s[i]);
+}
+for(const char of s){
+    console.log(char);
+}
+//for each loop does not work here 

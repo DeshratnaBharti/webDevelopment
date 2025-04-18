be1f1a1a97fdf251d7942beddf21582c9235cd4a
Deshratna Bharti <133379373+DeshratnaBharti@users.noreply.github.com>
2025-04-18 19:32:22 +0630

    Create templete_literal.js

---
+++ templete_literal.js
@@ -0,0 +1,11 @@
+// your code goes here
+let a ="Deshratna Bharti";
+let exp=1;
+console.log("my name is "+a+" and my exprence is "+exp);//this is not a correct way to write it ;
+console.log(`my name is ${a} and my experience is ${exp}`);//this is correct way
+
+//suppose we have to write Desh"ratna (how will you write it)
+//1st.using \
+console.log("Desh\"ratna");
+//2nd using backtick
+console.log(`Desh"ratna`);

7a6ff10e82e42c1dece67e3727871cc34183b7b2
Deshratna Bharti <133379373+DeshratnaBharti@users.noreply.github.com>
2025-04-21 09:49:32 +0630

    Create random number.js

---
+++ random number.js
@@ -0,0 +1,9 @@
+// your code goes here
+let num =Math.floor(Math.random()*10);//0 to 9
+console.log(num);
+let n =Math.floor(Math.random()*10+1);
+console.log(n);
+//write a code to generate a random integer between 0 and 10 (included)
+console.log(Math.floor(Math.random() * 11));//o se 10 tak random koi bhu element print karega
+//same -10 to 10
+console.log(Math.floor(Math.random() * 21) - 10); // -10 se 10 tak random numbers print karega

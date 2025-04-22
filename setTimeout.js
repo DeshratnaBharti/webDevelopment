68ca873b3002c981773017da6c35b5d70667de02
Deshratna Bharti <133379373+DeshratnaBharti@users.noreply.github.com>
2025-04-22 08:48:37 +0630

    Create setTimeout.js

---
+++ setTimeout.js
@@ -0,0 +1,10 @@
+
+function hello(){
+    console.log("hello");
+}
+function mello(){
+    
+    console.log("mello");
+}
+setTimeout(hello,2*1000);
+setTimeout(mello,1*1000);//pahle mello print hoga 

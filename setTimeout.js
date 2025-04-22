eab7af750a39aea0448cf89eca56fc02eedb27df
Deshratna Bharti <133379373+DeshratnaBharti@users.noreply.github.com>
2025-04-22 08:52:32 +0630

    Update setTimeout.js

--- setTimeout.js
+++ setTimeout.js
@@ -8,3 +8,7 @@ function mello(){
 }
 setTimeout(hello,2*1000);
 setTimeout(mello,1*1000);//pahle mello print hoga 
+//isko aise bhi likh sakte hai
+setTimeout(function() {
+    console.log("hello");
+}, 2*1000);
